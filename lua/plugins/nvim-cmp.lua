f822f44fe6e1aaa3484f5a75bcc9a5c0b12c1756
kuwifuwa <135856468+kuwifuwa@users.noreply.github.com>
2025-05-17 18:24:22 +0000

    Add nvim_lsp_signature_help for nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -4,6 +4,7 @@ return {
         "L3MON4D3/LuaSnip",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
     },
     opts = function ()
         local luasnip = require("luasnip")
@@ -29,6 +30,7 @@ return {
             sources = cmp.config.sources({
                 { name = "nvim_lsp" },
                 { name = "luasnip" },
+                { name = "nvim_lsp_signature_help" }
             }, {
                 {
                     name = "path",
