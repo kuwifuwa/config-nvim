a8de16d1daae08af6f672e67c5adc540b3540694
kuwifuwa <135856468+kuwifuwa@users.noreply.github.com>
2025-05-29 10:40:07 +0000

    feat: add "<Leader>sd" and "<Leader>sr" for *specific* LSP mappings

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,2 +1,4 @@
 local bufnr = vim.api.nvim_get_current_buf()
 vim.keymap.set("n", "gra", function () vim.cmd.RustLsp("codeAction") end, { buffer = bufnr })
+vim.keymap.set("n", "<Leader>sd", function () vim.cmd.RustLsp("renderDiagnostic") end, { buffer = bufnr })
+vim.keymap.set("n", "<Leader>sr", function () vim.cmd.RustLsp("relatedDiagnostics") end, { buffer = bufnr })
