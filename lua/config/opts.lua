d1276d56e8bd7165cf0ef6073132dde9bb8f64bf
kuwifuwa <135856468+kuwifuwa@users.noreply.github.com>
2025-09-05 09:19:49 +0000

    fix: add tabstop=4

--- lua/config/opts.lua
+++ lua/config/opts.lua
@@ -5,6 +5,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
 
+vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = -1 -- same as shiftwidth
