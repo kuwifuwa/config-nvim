087ca0a525a6450e26f515163547f5b2a07df971
kuwifuwa <135856468+kuwifuwa@users.noreply.github.com>
2025-10-20 15:39:16 +0000

    feat: add basedpyright config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,8 +1,18 @@
 return {
     "neovim/nvim-lspconfig",
     config = function (LazyPlugin, opts)
+        vim.lsp.config("basedpyright", {
+            settings = {
+                basedpyright = {
+                    analysis = {
+                        diagnosticMode = "workspace",
+                    }
+                }
+            }
+        })
         vim.lsp.enable("basedpyright")
         vim.lsp.enable("ruff")
+
         vim.lsp.enable("ts_ls")
     end,
 }
