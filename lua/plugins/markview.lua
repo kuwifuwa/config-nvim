e1a7c2918c489c97789f1e1c0c8f717ac5fecaf3
kuwifuwa <135856468+kuwifuwa@users.noreply.github.com>
2026-02-07 07:13:47 +0000

    fix: markview work with Avante

--- lua/plugins/markview.lua
+++ lua/plugins/markview.lua
@@ -1,4 +1,10 @@
 return {
     "OXY2DEV/markview.nvim",
     lazy = false,
+    opts = {
+        preview = {
+            filetypes = { "markdown", "quarto", "rmd", "typst", "asciidoc", "Avante" },
+            ignore_buftypes = {},
+        }
+    }
 }
